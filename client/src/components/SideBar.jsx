3cf7b4aaadeaf0cec3af8be0bfa5827a4b5ed5a5
Baby mumthas <babymumthas808@gmail.com>
2026-01-08 23:59:21 +0530

    add the image tag

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -46,7 +46,11 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
       <div className="flex flex-col">
         {userDummyData.map((user, index) => (
           <div>
-            <img src={user?.profilePic || assets.avatar_icon} alt="" />
+            <img
+              src={user?.profilePic || assets.avatar_icon}
+              alt=""
+              className="w-8.75 aspect-square rounded-full "
+            />
           </div>
         ))}
       </div>
