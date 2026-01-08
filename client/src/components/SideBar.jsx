df8c2e29721f29730692df1fe6631f4babe22643
Baby mumthas <babymumthas808@gmail.com>
2026-01-09 00:18:22 +0530

    update the user online offline status and message

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -33,7 +33,7 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
           </div>
         </div>
 
-        <div className="bg-[#282142] rounded-full flex items-centre gap-2 py-3 px-4 mt-5 ">
+        <div className="bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5 ">
           <img src={assets.search_icon} alt="search" className="w-3" />
           <input
             type="text"
@@ -45,12 +45,31 @@ const SideBar = ({ selectedUser, setSelectedUser }) => {
 
       <div className="flex flex-col">
         {userDummyData.map((user, index) => (
-          <div>
+          <div
+            key={index}
+            onClick={() => setSelectedUser(user)}
+            className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${
+              selectedUser?._id === user._id && "bg-[#282142]/50"
+            } `}
+          >
             <img
               src={user?.profilePic || assets.avatar_icon}
               alt=""
               className="w-8.75 aspect-square rounded-full "
             />
+            <div className="flex flex-col leading-5">
+              <p>{user.fullName}</p>
+              {index < 3 ? (
+                <span className="text-green-400 text-xs">Online</span>
+              ) : (
+                <span className="text-neutral-400 text-xs">Offline</span>
+              )}
+            </div>
+            {index > 2 && (
+              <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">
+                {index}
+              </p>
+            )}
           </div>
         ))}
       </div>
