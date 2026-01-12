94611d4a932b55dd13152181da6845173b2bbc51
Baby mumthas <babymumthas808@gmail.com>
2026-01-13 00:53:14 +0530

    add cloudinary to store images

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -54,4 +54,7 @@ export const Login = async (req, res) => {
     }
 }
 
-
+//controller to check if user is authenticated
+export const checkAuth = async (req, res) => {
+    res.json({ success: true, user: req.user });
+}
