76f0cb426a97a016fb221c39d6296cdd4ea027f2
Baby mumthas <babymumthas808@gmail.com>
2026-01-12 23:42:33 +0530

    database connected succsesfully

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import http from "http";
+import { connectDB } from "./lib/db.js";
 
 //create express app and http server
 const app = express();
@@ -13,6 +14,11 @@ app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 app.use("/api/status", (req, res) => res.send("Server is live"));
+
+
+//connect to database
+
+await connectDB();
 
 const PORT = process.env.PORT || 5000;
 
