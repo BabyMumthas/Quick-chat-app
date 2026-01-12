22aa4cf349b217c0a24bd278a79ebf1d7deb6b7c
Baby mumthas <babymumthas808@gmail.com>
2026-01-13 00:16:08 +0530

    create controller for login a user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,7 @@
+import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils";
+
+
 //SignUp a new user
 
 export const Signup = async () => {
@@ -7,12 +11,45 @@ export const Signup = async () => {
         if (!fullName || !email || !password || !bio) {
             return res.json({ success: false, message: "All fields are required" });
         }
-const user =await User.findOne({ email });
+        const user = await User.findOne({ email });
         if (user) {
             return res.json({ success: false, message: "User already exists" });
         }
 
-    } catch (error) {
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
+        const newUser = await User.create({
+            fullName,
+            email,
+            password: hashedPassword,
+            bio
+        });
+        const token = generateToken(newUser._id);
+
+        res.json({ success: true, userData: newUser, token, message: "Account created successfully" });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+
+
+    }
+}
+
+//controller for login a user
+export const Login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const userData = await User.findOne({ email });
+
+        const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+        if (!isPasswordCorrect) {
+            return res.json({ success: false, message: "Invalid credentials" });
+        }
+        const token = generateToken(userData._id);
+        res.json({ success: true, userData, token, message: "Login successful" });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
     }
 }
