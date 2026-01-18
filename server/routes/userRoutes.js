c55c7e7002a40539ddeed539525d950a57c0a1eb
Baby mumthas <babymumthas808@gmail.com>
2026-01-18 14:01:47 +0530

    create all api endpoints for user

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { checkAuth, Login, Signup, updateProfile } from '../controllers/userController';
+import { protectRoute } from '../middleware/auth.js';
+
+
+const userRoutes = express.Router();
+
+
+userRoutes.post("/signUp", Signup)
+userRoutes.post("/login", Login)
+userRoutes.put("/update-profile", protectRoute, updateProfile)
+userRoutes.get("/check", protectRoute, checkAuth);
